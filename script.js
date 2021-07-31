fdf3fb2f8e791a2956b8e875ad7d03545a51ed15
vang007 <suavang83@yahoo.com>
2021-07-31 00:11:42 -0500

    worked on js and html search box

--- script.js
+++ script.js
@@ -1,24 +1,44 @@
-javascript in progress
+
+let apiKey = "6ba1a36343cffd3493f18deba94fd15d";
 //    let api = "https://api.openweathermap.org/data/2.5/weather";      //
 //    let apiKey = "6ba1a36343cffd3493f18deba94fd15d";      //
 
-function getWeather() {
-    let temperature = document.getElementById("temperature");
-    let description = document.getElementById("description");
-    let location = document.getElementById("location");
-}
+var location = document.querySelector("city");
+var SubmitHandler = function (event) {
+    event.preventDefault();
+    console.log(event);
+};
+
+location.addEventListener("submit", SubmitHandler);
 
 
-let api = "http://api.openweathermap.org/data/2.5/";
-let apiKey = "6ba1a36343cffd3493f18deba94fd15d";
 
-navigator.geolocation.getCurrentPosition(success, error);
 
-function success(position) {
-    console.log(position)
-}
-function error() {
-    console.log("error")
-}
 
-getWeather;
+// function getWeather() {
+//     fetch(`https://api.openweathermap.org/data/2.5/weather?q=${"Dallas"}&appid=${apiKey}`)
+//         .then(function (res) {
+//             return res.json()
+//         }).then(function (data) {
+//             console.log(data);
+//         })
+// };
+
+
+// let temperature = document.getElementById("temperature");
+// let description = document.getElementById("description");
+// let location = document.getElementById("location");
+
+
+
+// let api = "http://api.openweathermap.org/data/2.5/";
+
+// navigator.geolocation.getCurrentPosition(success, error);
+// function success(position) {
+//     console.log(position)
+// }
+// function error() {
+//     console.log("error")
+// }
+
+getWeather()
